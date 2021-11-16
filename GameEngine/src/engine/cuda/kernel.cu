c31d0037155e545dd07b01bd4b50fcaf5d29d59a
Mendiax <254658@student.pwr.edu.pl>
2021-11-16 18:34:50 +0100

    reformat and update of packages

--- GameEngine/src/engine/cuda/kernel.cu
+++ GameEngine/src/engine/cuda/kernel.cu
@@ -1,8 +1,5 @@
 ﻿#include <engine/cuda/kernel.cuh>
 #include <engine/board.h>
-
-
-//#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
 #include <malloc.h>
@@ -12,7 +9,6 @@ __global__ void gpu::calculateKernel(bool* boxesStatusIn, bool* boxesStatusOut, 
 {
 	
 }
-
 
 void gpu::flipCellStatus(unsigned long long int x)
 {
@@ -28,8 +24,6 @@ void gpu::freeMemory()
 	cudaFree(dev_cellsStatusIn_p);
 	free(cellsStatusBuffer_p);
 
-	// cudaDeviceReset must be called before exiting in order for profiling and
-	// tracing tools such as Nsight and Visual Profiler to show complete traces.
 	cudaError_t cudaStatus;
 	cudaStatus = cudaDeviceReset();
 	if (cudaStatus != cudaSuccess) {
@@ -42,7 +36,6 @@ cudaError_t gpu::mallocMemory(unsigned long long int size)
 	
 	cellsStatusBuffer_p = (bool*)calloc(size, sizeof(bool));
 	cudaError_t cudaStatus;
-	// Choose which GPU to run on, change this on a multi-GPU system.
 	cudaStatus = cudaSetDevice(0);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
@@ -50,7 +43,6 @@ cudaError_t gpu::mallocMemory(unsigned long long int size)
 		return cudaStatus;
 	}
 
-	// Allocate GPU buffers for three vectors (two input, one output)    .
 	cudaStatus = cudaMalloc((void**)&dev_cellsStatusIn_p, size * sizeof(bool));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
@@ -77,14 +69,13 @@ cudaError_t gpu::mallocMemory(unsigned long long int size)
 	return cudaSuccess;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
 cudaError_t gpu::calculateWithCuda(bool* statusArray)
 {
 	cudaError_t cudaStatus;
 	unsigned long long int size = cellsStatusLength;
 	calculateKernel << <size, 8 >> > (dev_cellsStatusIn_p, dev_cellsStatusOut_p, size);
 
-	// Check for any errors launching the kernel
+
 	cudaStatus = cudaGetLastError();
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
@@ -92,15 +83,13 @@ cudaError_t gpu::calculateWithCuda(bool* statusArray)
 		return cudaStatus;
 	}
 
-	// cudaDeviceSynchronize waits for the kernel to finish, and returns
-	// any errors encountered during the launch.
 	cudaStatus = cudaDeviceSynchronize();
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
 		freeMemory();
 		return cudaStatus;
 	}
-	// Copy output vector from GPU buffer to host memory.
+
 	cudaStatus = cudaMemcpy(statusArray, dev_cellsStatusOut_p, size * sizeof(bool), cudaMemcpyDeviceToHost);
 
 	bool* buf = dev_cellsStatusIn_p;
