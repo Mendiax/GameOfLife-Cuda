ed0f400b9697d6bade1c8f107d6dac7fa5bd6276
Ksawery Możdżyński <254624@student.pwr.edu.pl>
2021-11-18 23:34:11 +0100

    dodanie shaderow w openGL

--- GraphicEngine/inc/graphic/painter.h
+++ GraphicEngine/inc/graphic/painter.h
@@ -1,21 +1,56 @@
 #ifndef PAINTER_H
 #define PAINTER_H
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <graphic/test.h>
-#include <GLFW/glfw3.h>
-
 #include "../../GameEngine/inc/engine/board.h"
 
 class Painter
 {
 public:
-	Painter();
-	void paint(Board board);
+	Painter(int width, int height);
+	~Painter();
+
+	int paint(Board& board);
+
 
 private:
 	GLFWwindow* window;
-	void paintSquare(float x, float y, float x2, float y2, float red, float green, float blue);
+
+	void createWindow();
+
+	void createShaders();
+
+	void createVertices();
+
+	void createBuffers();
+	
+	const int width;
+	const int height;
+	const int screen_width = 800;
+	const int screen_height = 600;
+
+	unsigned int shaderProgram;
+
+	unsigned int vertexBufferObject;
+	unsigned int vertexArrayObject;
+
+	const float defaultRed      = 0.47f;
+	const float defaultGreen    = 0.43f;
+	const float defaultBlue     = 0.00f;
+
+	const float aliveRed        = 0.90f;
+	const float aliveGreen      = 0.00f;
+	const float aliveBlue       = 0.00f;
+
+	const float backgroundRed   = 1.00f;
+	const float backgroundGreen = 0.98f;
+	const float backgroundBlue  = 0.00f;
+
+	const float backgroundAlpha = 1.0f;
+
+	float* vertices = 0;
+	int verticiesLength;
 };
 
 #endif
--- GraphicEngine/src/painter.cpp
+++ GraphicEngine/src/painter.cpp
@@ -1,80 +1,240 @@
-#include <GLFW/glfw3.h>
-#include <graphic/test.h>
-#include <GLFW/glfw3.h>
-
 #include "../inc/graphic/painter.h"
 #include <iostream>
+#include <stdexcept>
 
-Painter::Painter() {
-	/* Initialize the library */
-	if (!glfwInit())
-		return;
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return;
-	}
-
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
 
-void Painter::paint(Board board) {
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-		float x0, y0, windowWidth, windowHeight, tileWidth, tileHeight, margin;
+Painter::Painter(int width, int height) : width(width), height(height) {
+	createWindow();
 
-		windowWidth = windowHeight = 2;
+	createBuffers();
 
-		x0 = -windowWidth / 2;
-		y0 = -windowHeight / 2;
+	createShaders();
 
-		margin = 0.01;
+	createVertices();
+}
 
-		tileWidth = windowWidth / board.getWidth();
-		tileHeight = windowHeight / board.getHeight();
-
-		glBegin(GL_TRIANGLES);
-
-		board.getBoardArray()[10] = 0;
-
-		for (int i = 0; i < board.getWidth(); i++) {
-			for (int j = 0; j < board.getHeight(); j++) {
-
-				float red = 0.5f;
-				float green = 0.5f;
-				float blue = board.getBoardArray()[i * board.getWidth() + j] ? 1.0f : 0.0f;
-
-				paintSquare(x0 + i * tileWidth + margin, y0 + j * tileWidth + margin, x0 + (i + 1) * tileWidth - margin, y0 + (j + 1) * tileWidth - margin, red, green, blue);
-			}
-		}
-
-		glEnd();
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
-	}
+Painter::~Painter() {
+	delete[] vertices;
 
 	glfwTerminate();
 }
 
-void Painter::paintSquare(float x, float y, float x2, float y2, float red, float green, float blue) {
-	glColor3f(red, green, blue);
+int Painter::paint(Board& board) {
+	int shouldClose = glfwWindowShouldClose(window);
+	
+	if (!shouldClose)
+	{
+		for (int i = 0; i < board.getSize(); i++) {
+			if (board.getBoardArray()[i]) {
+				for (int k = 0; k < 6; k++) {
+					vertices[6 * i * 3 * 2 + 3 + 6 * k] = aliveRed;
+					vertices[6 * i * 3 * 2 + 4 + 6 * k] = aliveGreen;
+					vertices[6 * i * 3 * 2 + 5 + 6 * k] = aliveBlue;
+				}
+				
+			}
+			else {
+				for (int k = 0; k < 6; k++) {
+					vertices[6 * i * 3 * 2 + 3 + 6 * k] = defaultRed;
+					vertices[6 * i * 3 * 2 + 4 + 6 * k] = defaultGreen;
+					vertices[6 * i * 3 * 2 + 5 + 6 * k] = defaultBlue;
+				}
+			}
+		}
 
-	glVertex3f(x, y, 0);
-	glVertex3f(x, y2, 0);
-	glVertex3f(x2, y2, 0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * verticiesLength, vertices, GL_DYNAMIC_DRAW);
 
-	glVertex3f(x2, y2, 0);
-	glVertex3f(x2, y, 0);
-	glVertex3f(x, y, 0);
+
+		glClearColor(backgroundRed, backgroundGreen, backgroundBlue, backgroundAlpha);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vertexArrayObject);
+		glDrawArrays(GL_TRIANGLES, 0, verticiesLength / 6);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	return shouldClose;
+}
+
+void Painter::createWindow()
+{
+	if (!glfwInit()) {
+		throw std::exception("Failed to init GLFW.");
+	}
+
+	window = glfwCreateWindow(screen_width, screen_height, "Game of life", NULL, NULL);
+
+	if (!window)
+	{
+		throw std::exception("Failed to create GLFW window.");
+		glfwTerminate();
+
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		throw std::exception("Failed to initialize GLAD.");
+	}
+
+	glViewport(0, 0, screen_width, screen_height);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
+
+void Painter::createVertices()
+{
+	verticiesLength = width * height * 9 * 2 * 2; 
+	vertices = new float[verticiesLength];
+
+	const float screenWidth  = 2.0f;
+	const float screenHeight = 2.0f;
+
+	const float cellWidth  = screenWidth / width;
+	const float cellHeight = screenHeight / height;
+
+	const float margin = 0.01f;
+
+	const float x0 = -screenWidth / 2.0f;
+	const float y0 = -screenHeight / 2.0f;
+
+	int index = 0;
+
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+
+			vertices[index + 0] = x0 + i * cellWidth  + margin;
+			vertices[index + 1] = y0 + j * cellHeight + margin;
+			vertices[index + 2] = 0;
+			
+			vertices[index + 3] = defaultRed;
+			vertices[index + 4] = defaultGreen;
+			vertices[index + 5] = defaultBlue;
+
+			vertices[index + 6] = x0 + (i + 1) * cellWidth - margin;
+			vertices[index + 7] = y0 + j * cellHeight + margin;
+			vertices[index + 8] = 0;
+
+			vertices[index + 9] = defaultRed;
+			vertices[index + 10] = defaultGreen;
+			vertices[index + 11] = defaultBlue;
+
+			vertices[index + 12] = x0 + (i + 1) * cellWidth - margin;
+			vertices[index + 13] = y0 + (j + 1) * cellHeight - margin;
+			vertices[index + 14] = 0;
+
+			vertices[index + 15] = defaultRed;
+			vertices[index + 16] = defaultGreen;
+			vertices[index + 17] = defaultBlue;
+
+			vertices[index + 18] = x0 + i * cellWidth + margin;
+			vertices[index + 19] = y0 + j * cellHeight + margin;
+			vertices[index + 20] = 0;
+
+			vertices[index + 21] = defaultRed;
+			vertices[index + 22] = defaultGreen;
+			vertices[index + 23] = defaultBlue;
+
+			vertices[index + 24] = x0 + i * cellWidth + margin;
+			vertices[index + 25] = y0 + (j + 1) * cellHeight - margin;
+			vertices[index + 26] = 0;
+
+			vertices[index + 27] = defaultRed;
+			vertices[index + 28] = defaultGreen;
+			vertices[index + 29] = defaultBlue;
+
+			vertices[index + 30] = x0 + (i + 1) * cellWidth - margin;
+			vertices[index + 31] = y0 + (j + 1) * cellHeight - margin;
+			vertices[index + 32] = 0;
+
+			vertices[index + 33] = defaultRed;
+			vertices[index + 34] = defaultGreen;
+			vertices[index + 35] = defaultBlue;
+
+			index += 36;
+		}
+	}
+}
+void Painter::createBuffers()
+{
+	glGenBuffers(1, &vertexBufferObject);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+
+	glGenVertexArrays(1, &vertexArrayObject);
+	glGenBuffers(1, &vertexBufferObject);
+	glBindVertexArray(vertexArrayObject);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+void Painter::createShaders()
+{
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"layout (location = 1) in vec3 aColor;\n"
+		"out vec3 ourColor;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos, 1.0);\n"
+		"   ourColor = aColor;\n"
+		"}\0";
+
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"in vec3 ourColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(ourColor, 1.0f);\n"
+		"}\n\0";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glUseProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 }
